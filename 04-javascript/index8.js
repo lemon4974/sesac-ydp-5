660dda20ece104ea162093f21202e9e5fe9a70fb
lemon4974 <skyblue8323@gmail.com>
2023-07-26 18:52:13 +0900

    study: 내장 매서드

---
+++ 04-javascript/index8.js
@@ -0,0 +1,92 @@
+// 문자열 관련 내장 메서드
+let str1 = 'Strawberry Moon';
+let str2 = '    Strawberry Moon    ';
+
+// 문자열 인덱싱
+console.log(str1[0]);
+console.log(str1[0] + str1[12]);
+
+// Sonny
+console.log(str1[0] + str1[12] + str1[14] + str1[14] + str1[9]);
+
+// 문자열의 길이 (length는 메서드가 아닌 "속성")
+console.log(str1.length);
+console.log(str2.length);
+
+// 대/소문자 변환
+console.log(str1.toUpperCase());
+console.log(str1.toLowerCase());
+
+// 양끝 공백 제거
+console.log(str2.trim());
+console.log(str2.trim().length); // 'Strawberry Moon'.length 와 동일
+
+// indexOf() 글자 위치 찾기
+// 위치(index): 0부터 시작
+console.log(str1.indexOf('w'));
+console.log(str1.indexOf('r'));
+console.log(str1.indexOf('x'));
+// 존재하지 않는 문자에 대해 검색하면 -1 반환
+//어떤 문자열이 있는지 없는지 확인 시 사용
+
+// slice() 문자열 자르기
+console.log(str1.slice(11)); // 11 번 위치의 글자 ~ 끝
+console.log(str1.slice(1, 5)); // start(1) ~ end-1(4)까지 자르기
+
+// replace() 문자열 바꾸기
+console.log(str1.replace('a', 'x'));
+console.log(str1.replace('r', 'x'));
+console.log(str1.replaceAll('r', '*'));
+
+// split() 문자열 쪼개기 (배열로 변환)
+let date = '23.03.10';
+console.log(date.split('.'));
+console.log(date.split('3'));
+
+// repeat()
+console.log('hi'.repeat(5));
+
+////////////////////////////////////////
+// 배열 내장 메서드
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = ['quokka', 'rabbit', 'puppy', 'hamster'];
+
+// 배열에 값 추가
+// arr1[5] = 6; // arr1 배열의 5번 인덱스에 6이라는 값 추가
+// arr1[8] = 100; // 인덱스 건너뛰면 빈 값(empty)이 들어가게 됨
+
+// push(): 끝에 요소 추가
+arr1.push(6);
+arr1.push(10);
+console.log(arr1);
+
+// pop(): 맨 끝 요소 제거
+arr1.pop();
+console.log(arr1);
+
+// unshift(): 맨 앞에 요소 추가
+arr1.unshift('cat');
+console.log(arr1);
+
+// shift(): 맨 앞에 요소 제거
+arr1.shift();
+console.log(arr1);
+
+// includes(요소): 요소 있는지 없는지 검사 (true, false)
+console.log(arr2.includes('quokka'));
+console.log(arr2.includes('apple'));
+
+// reverse()
+console.log(arr1.reverse());
+console.log(arr1); // 원본 배열이 변경된 것
+
+// join(): join 안의 문자열 기준으로 병합
+console.log(arr2);
+console.log(arr2.join(''));
+console.log(arr2.join('-'));
+
+// 메서드 체이닝(method chaining)
+// 여러 메서드를 연결해서 사용할 수 있다!
+console.log('hello'.split(''));
+console.log('hello'.split('').reverse());
+console.log('hello'.split('').reverse().join(''));
