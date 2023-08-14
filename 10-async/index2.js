5d65e83894cbf4c6a1cddaa9df6b432f8de7ed07
lemon4974 <skyblue8323@gmail.com>
2023-08-14 16:16:39 +0900

    study: js 비동기 처리 - promise, async/await

---
+++ 10-async/index2.js
@@ -0,0 +1,156 @@
+// // // Promise(프로미스)
+// // // - 비동기 함수를 동기처리하기 위해 만든 객체
+// // // - 미래에 실패/성공에 대한 결과 값을 "약속" 한다는 의미
+// // // - 성공, 실패 분리해서 반환
+// // //      - 성공, 실패에 대한 결과는 then, catch 메서드로 이어 받아서 처리 가능
+// // // => 성공이든 실패든 무언가의 "최종 결과"
+// // // resolved: 성공
+// // // rejected: 실패
+
+// // // 1. promise를 생성하는 코드
+// // function promise1(flag) {
+// //   // flag false true를 위한 불리안 인자.
+// //   return new Promise(function (resolve, reject) {
+// //     if (flag) {
+// //       resolve(
+// //         `현재 프로미스의 상태는 fulfilled(이행)! then 메서드로 연결~ 이 때 flag 값은 ${flag}!`
+// //       );
+// //     } else {
+// //       reject(
+// //         `현재 프로미스의 상태는 rejected(거절)! catch 메서드로 연결~ 이 때 flag 값은 ${flag}!`
+// //       );
+// //     }
+// //   });
+// // }
+
+// // // 2. promise를 사용(소비)하는 코드
+// // // promise1(5 > 3)
+// // //   .then(function (result) {
+// // //     console.log(result);
+// // //   })
+// // //   .catch(function (error) {
+// // //     console.log(error);
+// // //   });
+
+// // //화살표 함수로도 가능!
+// // promise1(5 < 3)
+// //   .then((result) => console.log(result))
+// //   .catch((error) => console.log(error));
+
+// // //////////////////////////////////////////////
+// // // 2. 프로미스 예제
+// // //  index.js에서 "콜백 함수"를 이용해 동기처리한 것을 "promise" 를 이용해 동기처리
+// // function goMart() {
+// //   console.log('마트에 가서 어떤 음료를 살지 고민한다.');
+// // }
+
+// // function pickDrink() {
+// //   return new Promise(function (resolve, reject) {
+// //     setTimeout(function () {
+// //       console.log('고민 끝!!');
+// //       product = '제로 콜라';
+// //       price = 4000; //2000, 4000
+// //       //resolve(); //resolve 안에 내용 없어도 무방. 성공으로 동기 처리한 것. 내용을 입력하면 resolve에 대한 어떤 결과가 입력된 것.
+// //       if (price <= 2000) {
+// //         resolve();
+// //       } else {
+// //         reject();
+// //       }
+// //     }, 3000);
+// //   });
+// // }
+
+// // function pay() {
+// //   console.log(`상품명: ${product}, 가격 ${price}`);
+// // }
+
+// // function nopay() {
+// //   console.log('금액 부족 ㅜㅜ ');
+// // }
+
+// // let product;
+// // let price;
+// // goMart();
+// // pickDrink().then(pay).catch(nopay); //아래 코드와 동일
+// // // pickDrink().then(function () {
+// // //   pay();
+// // // });
+// // // then으로 연결 처리.
+
+// ////////////////////////////////////////////////////////////
+// // 3. 프로미스 체이닝(chaining)
+// // 함수를 이용해 (4 + 3)*2 - 1=  13을 연산해보자!
+// //  sub( mul( add(4, 3), 2), 1): add -> mul-> sub
+
+// // case1. 콜백함수로 처리한다면?
+// function add(n1, n2, callback) {
+//   setTimeout(function () {
+//     const result = n1 + n2; /// 7
+//     callback(result); // callback(7)
+//   }, 1000);
+// }
+// function mul(n, callback) {
+//   setTimeout(function () {
+//     const result = n * 2; //14
+//     callback(result); // callback(14)
+//   }, 700);
+// }
+// function sub(n, callback) {
+//   setTimeout(function () {
+//     const result = n - 1; //7
+//     callback(result); // callback(7)
+//   }, 500);
+// }
+
+// add(4, 3, function (x) {
+//   console.log('1: ', x); // x = 7
+//   mul(x, function (y) {
+//     console.log('2: ', y); // y = 14
+//     sub(y, function (z) {
+//       console.log('3: ', z); // z = 13
+//     });
+//   });
+// });
+
+// case
+function add(n1, n2) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      const result = n1 + n2;
+      resolve(result);
+    }, 1000);
+  });
+}
+function mul(n) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      const result = n * 2; //14
+      //   resolve(result); //resolve(14)
+      reject(new Error('의도적으로 발생시킨 에러!!!'));
+    }, 700);
+  });
+}
+function sub(n) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      const result = n - 1;
+      resolve(result);
+    }, 500);
+  });
+}
+
+add(4, 3)
+  .then(function (result) {
+    console.log('1: ', result); // 7
+    return mul(result); //return mul(7)
+  })
+  .then(function (result) {
+    console.log('2: ', result); // 14
+    return sub(result); //return sub(14)
+  })
+  .then(function (result) {
+    console.log('3: ', result); // 13
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
---
+++ 10-async/index3.js
@@ -0,0 +1,92 @@
+// // async function f1() {
+// //   return 1;
+// // }
+// // // async 가 붙은 함수는 항상 프로미스를 반환
+// // async function f2() {
+// //   return Promise.resolve(1);
+// // }
+
+// // console.log('1 >> ', f1()); // f1 함수는 async 키워드가 붙어 있으므로 프로미스를 반환! => Promise { 1 }
+// // f1().then(function (result) {
+// //   console.log('2 >> ', result); // 1
+// // });
+
+// // console.log('3 >> ', f2()); //  Promise { <pending> }
+// // f2().then(function (result) {
+// //   console.log('4 >> ', result); // 1
+// // });
+
+// //////////////////////////////////////////////////
+// // async - await
+// // async: 함수 앞에 붙이는 키워드
+// // - 함수만 보고도 비동기 함수임을 유추하고자 함
+// // - 프로미스를 반환
+
+// // await: 기다리다
+// // - 성공/실패로 프로미스 객체의 실행이 완료되기를 기다림
+// // - await 뒤에는 프로미스가 오게 됨
+// // - **async 키워드를 사용한 함수 안에서만 await 키워드를 사용 가능
+
+// // async/await 세트
+
+// // 1초 뒤에 과일 배열을 출력하는 코드
+// function fetchFruits() {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       const fruits = ['사과', '레몬', '수박'];
+//       resolve(fruits);
+//       //   reject(new Error('알 수 없는 에러 발생!! 아이템을 가져올 수 없음!!'));
+//     }, 1000);
+//   });
+// }
+
+// // 1) promise then() 메서드 사용
+// // fetchFruits()
+// //   .then(function (f) {
+// //     console.log(f);
+// //   })
+// //   .catch(function (error) {
+// //     console.log(error);
+// //   });
+
+// // async-await 키워드 사용시
+// // 에러처리는 try-catch 구문으로 하게 됨!
+// async function printItems() {
+//   try {
+//     const fruits = await fetchFruits();
+//     // fetchFruits가 실행될 때까지 기다림. 실행 후 그 결과를 fruits에 저장
+//     console.log(fruits);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// printItems();
+
+function goMart() {
+  console.log('마트에 가서 어떤 음료를 살지 고민한다.');
+}
+
+function pickDrink() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      // 3초 기다린 후에 코드 실행(= 3초 고민함)
+      console.log('고민 끝!!');
+      product = '제로 콜라';
+      price = 2000;
+    }, 3000);
+  });
+}
+
+function pay() {
+  console.log(`상품명: ${product}, 가격 ${price}`);
+}
+
+async function exec() {
+  goMart();
+  await pickDrink();
+  pay();
+}
+
+let product;
+let price;
+exec();
