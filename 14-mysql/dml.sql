c94472978e563b206fe5890a7b1018f526d32a52
lemon4974 <skyblue8323@gmail.com>
2023-08-25 14:25:39 +0900

    study: sql select

--- 14-mysql/dml.sql
+++ 14-mysql/dml.sql
@@ -64,7 +64,7 @@ insert into customer values
 -- UPDATE 수정
 -- custid가 happy인 고객의 주소를 대한민국 서울로 변경
 update customer set addr = '대한민국 서울' where custid = 'happy';
-
+drop table orders;
 
 -- 여러 튜플 동시 추가
 -- insert into customer values;
@@ -131,6 +131,7 @@ select custid, custname, addr, phone, birth from customer;
 
 -- * 와일드카드 사용
 select * from customer;
+select * from orders;
 
 -- 고객 테이블에 있는 모든 주소 검색
 select addr from customer;
@@ -201,8 +202,102 @@ select * from customer where addr like '미국%' and birth >= '영국%';
 -- 휴대폰 번호 마지막 자리가 4가 아닌 고객 검색
 select * from customer where phone NOT LIKE '%_4';
 
+-- < ORDER BY >
+-- order by 없으면 pk 기준 오름차순 정렬
+select * from customer;
 
+select * from customer order by custname; -- asc가 default
 
+select * from customer order by custname desc;
+
+-- where 절과 order by 함께 사용 (단, 이 때 order by 가 where 보다 뒤에 위치해야 함)
+-- 2000년생 이후 출생자 중에서 주소를 기준으로 내림차순 검색
+select * from customer where birth >= '2000-01-01' order by addr desc;
+-- select * from customer order by addr desc where birth >= '2000-01-01'; -- error: 구문 순서 오류
+
+-- 2000년생 이후 출생자 중에서 주소를 기준으로 내림차순 그리고 id 기준으로 내림차순 검색
+select * from customer where birth >= '2000-01-01' order by addr desc, custid desc;
+-- 2000년생 이후 출생자 중에서 주소를 기준으로 오름차순 그리고 id 기준으로 내림차순 검색
+select * from customer where birth >= '2000-01-01' order by addr, custid desc;
+
+-- < LIMIT >
+-- 행의 개수를 제한
+select * from customer where birth >= '2000-01-01' limit 2;
+select * from customer limit 3;
+
+-- < 집계 함수 >
+-- 계산하여 어떤 값을 리턴하는 "함수"
+-- group by 절과 함께 쓰이는 케이스가 많음
+
+-- 주문 테이블에서 총 판매 개수 검색
+select sum(amount) from orders;
+
+-- 주문 테이블에서 총 판매 개수 검색 + 의미있는 열이름으로 변경 (total_sales)
+select sum(amount) as 'total_amount' from orders;
+
+-- 주문 테이블에서 총 판매 개수, 평균 판매 개수, 상품 최저가, 상품 최고가 검색
+select sum(amount) as 'total_amount',
+avg(amount) as 'avg_amount', min(price) as 'min_amount', max(price) as 'max_amount' 
+from orders;
+    
+-- 주문 테이블에서 총 주문 건수 (= 튜플 개수)
+select count(*) from orders;
+
+-- 주문 테이블에서 주문한 고객 수(중복 없이)
+select count(distinct custid) from orders;
+
+-- < GROUP BY >
+-- "별로"
+
+-- 고객별로 주문한 주문 건수 구하기
+select custid, count(*) from orders group by custid;
+
+-- 고객별로 주문한 상품 총 수량 구하기
+select custid, sum(amount) from orders group by custid;
+
+-- 고객별로 주문한 총 주문액 구하기
+select custid, sum(price * amount) from orders group by custid;
+
+-- 상품별 판매 개수 구하기
+select prodname, sum(amount)from orders group by prodname;
+
+-- < HAVING >
+-- group by 절 이후 추가 조건
+
+-- 총 주문액이 10000원 이상인 고객에 대해서 고객별로 주문한 상품 총 수량 구하기
+select custid, sum(amount), sum(price*amount) from orders 
+    group by custid
+    having sum(price*amount) >= 10000;
+    
+/*select custid, sum(amount), sum(price*amount) from orders 
+	where sum(price*amount) >= 10000
+    group by custid; -- error code 1111. group 함수 잘못 사용*/
+
+-- 총 주문액이 10000원 이상인 고객에 대해서 고객별로 주문한 상품 총 수량 구하기
+-- (단, custid가 'bunny'인 고객은 제외하고 출력할 것)
+-- where, group by, having 모두 사용한 예시alter
+select custid, sum(amount), sum(price * amount) from orders
+	where custid != 'bunny'
+    group by custid
+    having sum(price * amount) >= 10000;
+    
+-- group by 주의 사항
+-- select 절에서 group by에서 사용한 속성과 집계함수만 사용 가능
+-- 고객별로 주문한 주문 건수 구하기
+select custid, count(*) from orders group by custid;
+select count(*) from orders group by custid;
+
+-- where vs having 
+
+-- having
+-- 그룹에 대해서 필터링(그래서 group by 함께 쓰임)
+-- group by 보다 뒤에 위치
+-- 집계함수랑 함께 사용 가능
+
+-- where
+-- 각각의 행을 필터링
+-- group by 보다 앞에 위치
+-- 집계함수를 쓸 수는 있으나 having 처럼 자유롭게 쓸 수는 없음 
 
 
 
