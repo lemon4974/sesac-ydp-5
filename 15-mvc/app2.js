ef1da1aa276ca33dfb32dca6f68720df9fe29012
lemon4974 <skyblue8323@gmail.com>
2023-08-28 12:54:29 +0900

    study: MVC 패턴 실습, 임의의 사이트

--- 15-mvc/app2.js
+++ 15-mvc/app2.js
@@ -13,9 +13,9 @@ app.use(express.json());
 const indexRouter = require('./routes/index2');
 app.use('/', indexRouter);
 
-// app.get('*', (req, res) => {
-//   res.render('404');
-// });
+app.get('*', (req, res) => {
+  res.render('404');
+});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
--- 15-mvc/controller/pr-control.js
+++ 15-mvc/controller/pr-control.js
@@ -1,7 +1,7 @@
 // Controller
 // 주로 함수가 저장된다. 어떤 것이 실행되어야 하는지!
 const info = require('../model/pr-model.js');
-// const Comment = {getCommentAll: function}
+// const sea = (getmsgAll: function);
 // Comment.js 파일은 getCommentAll 이 export되는 파일로 DB와 연결되는 모델 파일
 
 exports.capital = (req, res) => {
@@ -16,13 +16,13 @@ exports.info_all = (req, res) => {
 // 위 함수는 comments를 render. 위의 모듈로 Comment에 객체로 정보를 가져온 후, Comment.getCommentAll()를 접근?
 exports.info = (req, res) => {
   const seaId = Number(req.params.id);
-  const comments = Comment.getmsgAll();
+  const seainfo = info.getmsgAll();
   // '/comment/:id'로의 경로로 이 함수에 접근(?)시 이 함수가 실행되도록 함.
   // 위 함수는 comments를 render. 위의 모듈로 Comment에 객체로 정보를 가져온 후, Comment.getCommentAll()를 접근?
 
-  if (!comments[seaId - 1]) {
+  if (!seainfo[seaId - 1]) {
     return res.render('404');
   }
 
-  res.render('info', { info: comments[seaId - 1] });
+  res.render('info', { info: seainfo[seaId - 1] });
 };
