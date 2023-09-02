d5af2773fbdaab3c8a6f07199eb089e19a57fe3a
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-09-02 17:37:28 +0900

    study: mvc, mysql -> sequelize

--- 17-mvc_mysql_sequelize/controller/Cuser.js
+++ 17-mvc_mysql_sequelize/controller/Cuser.js
@@ -36,42 +36,60 @@ exports.postSignin = async (req, res) => {
     const result = await User.findOne({
       where: { userid, pw, name },
     });
-    res.send(result);
+    console.log('postSignin_result>>>', result);
+
+    // res.send(result);
+    if (result) {
+      res.send(true);
+    } else {
+      res.send(false);
+    }
   } catch (err) {
-    res.send(false);
+    res.send('404'); //다른부분...여기를 왜 false로 받았지
   }
+  console.log('postSignin >>>', userid, pw);
 };
 
 exports.postProfile = async (req, res) => {
   // User.postProfile(req.body.userid, (result) => {
   //   res.render('profile', { data: result[0] });
   // });
-  const { userid } = req.body;
-  const result = await User.findOne({
-    where: { id: userid },
-  });
+  // const { userid } = req.body;
+  try {
+    const result = await User.findOne({
+      where: { userid: req.body.userid },
+    });
+    console.log('postProfile_result>>>', result);
 
-  res.render('profile', {
-    user: {
-      // id: result.id,
-      userid: result.userid,
-      pw: result.pw,
-      name: result.name,
-    },
-  });
+    res.render('profile', { data: result });
+  } catch (err) {
+    res.render('404');
+    console.log('postProfile_id>>>', req.body.id);
+  }
 };
 
 exports.editProfile = async (req, res) => {
-  const { id, userid, pw, name } = req.body;
-  const result = await User.update({ userid, pw, name }, { where: { id } });
-
-  res.send(true);
+  // const { id, userid, pw, name } = req.body;
+  const result = await User.update(
+    {
+      name: req.body.name,
+      pw: req.body.pw,
+    },
+    {
+      where: { id: req.body.id },
+    }
+  );
+  console.log(result);
+  res.send({ isUpdated: true });
 };
 
 exports.deleteProfile = async (req, res) => {
-  const { id } = req.body;
+  console.log('아이디값 ' + req.body.id);
   const result = await User.destroy({
-    where: { id },
+    where: { id: req.body.id },
   });
-  res.send(true);
+
+  console.log('result >>>', result);
+
+  res.send(true); //삭제 성공을 프론트로 넘김
 };
